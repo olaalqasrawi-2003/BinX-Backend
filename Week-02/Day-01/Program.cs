fa2200198a02b386413ab8f48cb36e7db7397f7e
Ola AlQasrawi <ola.alqasrawi2003@gmail.com>
2026-07-26 17:14:46 +0300

    Restore Week 2 Day 1

---
+++ Week-02/Day-01/Program.cs
@@ -0,0 +1,25 @@
+﻿Repository<User> userRepository = new Repository<User>();
+
+userRepository.Add(new User("Ola"));
+userRepository.Add(new User("Ahmad"));
+
+Console.WriteLine("Users:");
+
+foreach (User user in userRepository.GetAll())
+{
+    Console.WriteLine(user.Name);
+}
+
+Console.WriteLine();
+
+Repository<Product> productRepository = new Repository<Product>();
+
+productRepository.Add(new Product("Laptop"));
+productRepository.Add(new Product("Mouse"));
+
+Console.WriteLine("Products:");
+
+foreach (Product product in productRepository.GetAll())
+{
+    Console.WriteLine(product.Name);
+}
