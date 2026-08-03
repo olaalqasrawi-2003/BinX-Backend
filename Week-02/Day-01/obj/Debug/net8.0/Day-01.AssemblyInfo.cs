28807282f41ad985cb3523d50d86dc89a776100c
Ola AlQasrawi <ola.alqasrawi2003@gmail.com>
2026-08-03 17:27:45 +0300

    Week-03 Day-02 - Schema Design & Normalization

--- Week-02/Day-01/obj/Debug/net8.0/Day-01.AssemblyInfo.cs
+++ Week-02/Day-01/obj/Debug/net8.0/Day-01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day-01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17be33da449979103f1febde582ad2c2f9dcc05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3685a9f56f2dcd394f2313aaed7fb3861fddad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day-01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day-01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
